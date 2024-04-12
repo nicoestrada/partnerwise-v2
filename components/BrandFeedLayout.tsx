7a0b588b0715db3f5867b7701cf68c2482509dda
Nico Estrada <nicoestrada@Nicos-MBP.attlocal.net>
2024-04-11 19:15:16 -0500

    cards update

--- components/BrandFeedLayout.tsx
+++ components/BrandFeedLayout.tsx
@@ -33,7 +33,7 @@ export default function BrandFeedLayout() {
 
     return (
         <>
-            <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 mx-auto px-8 gap-4 md:gap-6 my-8 mx-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mx-auto gap-4 md:gap-6 my-8 mx-8">
                 {brands.map((brand) => (
                     <Card brand={brand} />
                 ))}
--- components/Card.tsx
+++ components/Card.tsx
@@ -2,17 +2,14 @@ export default function Card({ brand }) {
 
     return (
         <>
-            <div className="card mx-auto my-auto w-auto card-bordered my-8 mx-8 hover:bg-base-300 hover:cursor-pointer">
-                <figure className="w-fit"><img src={brand["OG image"]} alt={brand["URL"]+" hero image"} className="rounded-lg w-10/12" /></figure>
-                <div className="card-body bg-transparent p-5 flex">
-                    <h2 className="card-title  flex justify-between">
-                        {brand["URL"]}<div className="badge badge-primary">NEW</div>
-                    </h2>
-                    <p>{brand["Meta description"]}</p>
-                    <div className="card-actions justify-start">
-                        <div className="badge badge-outline">Fashion</div> 
-                        <div className="badge badge-outline">Products</div>
-                    </div>
+            <div className="card w-96 mx-auto my-8 mx-8 hover:cursor-pointer hover:shadow-xl">
+                <figure className="relative">
+                    <img src={brand["OG image"]} alt={brand["URL"]+" hero image"} className="rounded-xl w-96 h-80"/>
+                    <div className="badge bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300 absolute m-2 top-0 left-0">{brand["Category"]}</div>     
+                </figure>
+                <h2 className="font-semibold p-2">{brand["URL"]}</h2>
+                <p className="text-sm p-2">Est. revenue: {brand["Estimated annual revenue (lower bound)"].trim()}</p>
+                <div className="card-actions justify-start p-2">
                 </div>
             </div>
         </>
