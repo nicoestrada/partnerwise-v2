3d12934ec9d620d784436c10efd8c74896c4f042
Nico Estrada <nicoestrada@Nicos-MBP.attlocal.net>
2024-04-12 13:43:10 -0500

    latest

--- components/Header.tsx
+++ components/Header.tsx
@@ -43,7 +43,7 @@ const Header = () => {
   }, [searchParams]);
 
   return (
-    <header className="bg-base-100 z-40 top-0 sticky border-b">
+    <header className="bg-base-100 z-40 top-0 sticky border-b" data-scroll="1">
       <nav
         className="container flex items-center justify-between px-8 py-4 mx-auto top-0 z-50"
         aria-label="Global"
