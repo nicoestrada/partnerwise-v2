7810484113686afa34f6154b5858aace5dc666d2
Nico <estradanicolas@gmail.com>
2024-04-15 09:29:17 -0500

    tab list hover

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -107,11 +107,11 @@ const Tabs: React.FC = () => {
         <button
           onClick={() => handleTabClick(0)}
           className={`px-4 py-2 ml-2 font-medium ${
-            activeTab === 0 ? 'text-gray-800 border-b-2 border-blue-500' : 'text-gray-500'
+            activeTab === 0 ? 'text-gray-800 border-b-2 border-blue-500' : 'text-gray-400 font-gray-500 hover:text-gray-800 hover:border-b-2 hover:cursor-pointer hover:bg-base-200'
           }`}
         >
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-shirt mx-auto"><path d="M20.38 3.46 16 2a4 4 0 0 1-8 0L3.62 3.46a2 2 0 0 0-1.34 2.23l.58 3.47a1 1 0 0 0 .99.84H6v10c0 1.1.9 2 2 2h8a2 2 0 0 0 2-2V10h2.15a1 1 0 0 0 .99-.84l.58-3.47a2 2 0 0 0-1.34-2.23z"/></svg>
-            <p className="text-slate-800 text-xs mt-2 mx-auto">Apparel</p>
+            <p className="text-gray-500 text-xs mt-2 mx-auto hover:text-gray-800 ">Apparel</p>
         </button>
         <button
           onClick={() => handleTabClick(1)}
