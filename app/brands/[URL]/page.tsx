0ba99bd9e0111a4e870e429cb901e712c799dbfd
Nico <estradanicolas@gmail.com>
2024-04-17 09:17:45 -0500

    links on brand page

--- app/brands/[URL]/page.tsx
+++ app/brands/[URL]/page.tsx
@@ -50,19 +50,36 @@ export default function BrandPage({ params }: {params: {URL: string}}) {
         <>
             <Header />
             <div className="max-w-screen-xl mx-auto p-5">
-                <div className="p-4">
-                    <Link href={`https://`+brand["URL"]} className="no-underline hover:underline text-2xl">{brand["URL"]}</Link>
+                <div className="flex p-4 justify-between items-center">
+                    <h2 className="text-3xl font-semibold">{brand["Store name"]}</h2>
+                    <div className="flex items-center space-x-4"> 
+                        <div className="flex flex-row items-center font-medium"> 
+                            <Link href={`https://` + brand["URL"]} className="hover:border-b hover:border-gray-800 flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 m-1">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
+                                </svg>
+                                <p>Link</p> 
+                            </Link>
+                        </div>
+                        <div className="flex flex-row items-center font-medium">
+                            <Link href="" className="hover:border-b hover:border-gray-800 flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 m-1">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                                </svg>
+                                <p>Save</p> 
+                            </Link>
+                        </div>
+                    </div>
                 </div>
                 <div className="bg-white rounded-lg shadow-lg overflow-hidden border border-gray-300">
                     <div className="relative w-full h-64">
-                    <img
-                        src={brand["OG image"]}
-                        alt="Venue"
-                        className="absolute inset-0 w-full h-full object-cover object-center" // Updated classes here
-                    />
+                        <img
+                            src={brand["OG image"]}
+                            alt="Venue"
+                            className="absolute inset-0 w-full h-full object-cover object-center" />
                     </div>
                 </div>
-                </div>
+            </div>
             {/* <div className="flex flex-col max-w-screen-xl mx-auto p-5 lg:flex-row">
                 <div className="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">Good pitch ✅</div> 
                 <div className="divider lg:divider-horizontal">VS</div> 
