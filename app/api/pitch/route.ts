d6f1e53583b8bd7fe10a21343fc39b92c5521f8e
Nico Estrada <nicoestrada@Nicos-MBP.attlocal.net>
2024-04-17 15:58:44 -0500

    PitchGenerator completion

--- app/api/pitch/route.ts
+++ app/api/pitch/route.ts
@@ -1,23 +1,21 @@
 // pages/api/generatePitch.ts
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 import { OpenAI } from "openai";
 
 // Instantiate the OpenAI client
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
+    apiKey: process.env.OPENAI_KEY
 });
 
-export default async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: NextRequest, res: NextResponse) {
     if (req.method !== "POST") {
-        res.status(405).json({ error: "Method not allowed" });
-        return;
+        NextResponse.json({ error: "Method not allowed" });
     }
 
     const { brand } = req.body;
 
     if (!brand || !brand["URL"]) {
-        res.status(400).json({ error: "Brand URL is required" });
-        return;
+        NextResponse.json({ error: "Brand URL is required" });
     }
 
     try {
@@ -25,20 +23,20 @@ export default async function POST(req: NextApiRequest, res: NextApiResponse) {
             model: "gpt-3.5-turbo",
             messages: [{
                 role: "user",
-                content: `Write a pitch as a UGC creator to ${brand["URL"]} on how you would like to collaborate.`
+                content: `Write a pitch as a UGC creator to on how you would like to collaborate.`
             }],
             temperature: 0.6,
             max_tokens: 100
         });
 
         if (completion.choices.length > 0) {
-            res.status(200).json({ pitch: completion.choices[0].message.content });
+            return NextResponse.json({ pitch: completion.choices[0].message.content });
         } else {
-            res.status(500).json({ error: "No response from AI." });
+            return NextResponse.json({ error: "No response from AI." });
         }
     } catch (error) {
         console.error('Error calling OpenAI:', error);
-        res.status(500).json({ error: "Error fetching response from AI." });
+        return NextResponse.json({ error: "Error fetching response from AI." });
     }
 }
 
--- components/PitchGenerator.tsx
+++ components/PitchGenerator.tsx
@@ -3,8 +3,11 @@ import React, { useState } from 'react';
 
 const PitchGenerator = ({ brand }) => {
     const [generatedText, setGeneratedText] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    const [copyButtonText, setCopyButtonText] = useState("Copy")
 
     const handleGenerate = async () => {
+        setIsLoading(true);
         if (!brand || !brand["URL"]) {
             setGeneratedText("Brand URL is required for generating the pitch.");
             return;
@@ -18,7 +21,7 @@ const PitchGenerator = ({ brand }) => {
         };
 
         try {
-            const response = await fetch('/api/generatePitch', {
+            const response = await fetch('http://localhost:3000/api/pitch', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -39,8 +42,26 @@ const PitchGenerator = ({ brand }) => {
             }
         } catch (error) {
             console.error('Failed to generate pitch:', error);
-            setGeneratedText("Failed to communicate with the API.");
+            setGeneratedText("Failed to communicate with the API."+ error);
         }
+        setIsLoading(false);
+    };
+
+    const loadingIndicator = 
+    // Render a loading state while brands are being fetched
+      <div className="mx-auto justify-center">
+        <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin-slow dark:text-gray-600 fill-purple-500" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+        </svg>
+      </div>;
+    
+    const copyToClipboard = (generatedText: string) => {
+        navigator.clipboard.writeText(generatedText).then(() => {
+            setCopyButtonText("Copied ✅")
+        }).catch(err => {
+            console.error('Could not copy text: ', err);
+        });
     };
 
     return (
@@ -55,30 +76,40 @@ const PitchGenerator = ({ brand }) => {
                     </div>
                     <div className="flex items-center">
                             <a href={brand["Instagram page URL"]} target="_blank" className="mx-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-instagram"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-instagram"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg>
                             </a>
                             <a href={brand["LinkedIn page URL"]} target="_blank" className="mx-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>                            
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>                            
                             </a>
                             <a href={`mailto:${brand["Emails from website"]}`} className="mx-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail"><rect width="20" height="16" x="2" y="4" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg>                            
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-mail"><rect width="20" height="16" x="2" y="4" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg>                            
                             </a>
                     </div>
                 </div>
                 <textarea
                     id="inputText"
-                    className="textarea shadow-lg textarea-bordered h-24 w-full"
+                    className="textarea shadow-lg textarea-bordered h-60 w-full"
                     placeholder="Generated pitch will appear here..."
                     readOnly
                     value={generatedText}
-                >
-
-                </textarea>
+                />
+                <div className="flex justify-between items-center w-full mb-2">
+                    <div className="flex items-center">
+                        
+                    </div>
+                    <div className="flex items-center">
+                        <button
+                            className="btn mt-2 -mb-4 bg-green-200 hover:bg-green-600"
+                            onClick={() => copyToClipboard(generatedText)}
+                        >{copyButtonText}</button>
+                    </div>
+                </div>
+                
             </div>
 
             <div className="w-full px-2 my-2 lg:w-auto lg:order-last lg:flex-grow-0 lg:px-4">
                 <button className="btn bg-gradient-to-br from-blue-400 to-purple-600 text-white hover:shadow-xl w-full lg:w-auto" onClick={handleGenerate}>
-                    Generate 🤖
+                    {isLoading ? loadingIndicator : "Generate 🤖"} 
                 </button>
             </div>
         </div>
