aa4e8f698113807ff9e53eaf16ea44aeca9feeca
Nico Estrada <nicoestrada@Nicos-MBP.attlocal.net>
2024-04-23 18:22:06 -0500

    trying to remove color

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,12 +9,12 @@ import "./globals.css";
 
 const font = Inter({ subsets: ["latin"] });
 
-export const viewport: Viewport = {
-  // Will use the primary color of your theme to show a nice theme color in the URL bar of supported browsers
-  themeColor: config.colors.main,
-  width: "device-width",
-  initialScale: 1,
-};
+// export const viewport: Viewport = {
+//   // Will use the primary color of your theme to show a nice theme color in the URL bar of supported browsers
+//   themeColor: config.colors.main,
+//   width: "device-width",
+//   initialScale: 1,
+// };
 
 // This adds default SEO tags to all pages in our app.
 // You can override them in each page passing params to getSOTags() function.
